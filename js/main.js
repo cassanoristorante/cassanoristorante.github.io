e823b49e21829aa1875595dbebabcba76fd82c08
cinqueterreli <Anthony@Anthonys-MBP.home>
2016-09-01 15:31:59 -0400

    weee

--- js/main.js
+++ js/main.js
@@ -6,7 +6,24 @@ $(document).ready(function(){
 /* ------ LOAD MENUS ON CLICK ------- */
 
 
-$ function load_menu(menu){
+$(function load_menu(menu){
             document.getElementById("content").innerHTML='<object type="type/html" data='+menu+'></object>';
 
-  }
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
