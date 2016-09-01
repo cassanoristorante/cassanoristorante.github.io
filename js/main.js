8d617f58dd55f9c93b9823f1d9c0b2c3f3ebfbe1
cinqueterreli <Anthony@Anthonys-MBP.home>
2016-09-01 15:20:55 -0400

    YAY THINGS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 
+/* ------ LOAD MENUS ON CLICK ------- */
+
+
+
+function load_menu(menu){
+            document.getElementById("content").innerHTML='<object type="type/html" data='+menu+'></object>';
+  }
