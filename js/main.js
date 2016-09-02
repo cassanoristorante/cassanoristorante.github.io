6601819b4039ccc8a8b39930752873b0c75fc34f
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2016-09-02 13:15:24 -0400

    wee try it

--- js/main.js
+++ js/main.js
@@ -6,22 +6,14 @@ $(document).ready(function(){
 /* ------ LOAD MENUS ON CLICK ------- */
 
 
-/*  window.onload = function() {
-    document.getElementsByClassName("menu-link");.onclick = function loadMenu(menu) {
-        document.getElementById("menu-loader").innerHTML='<object type="type/html" data='+menu+'></object>';
-	};
 
-}; */
 
-$(function() {
+var getTime = function(time) {
+	var time = Date.getHours();
+	console.log(time);
+};
 
-    $('.menu-link').click(function(event) {
-        $event.preventDefault();
-        ('#target').load($this.attr('href'));
-        console.log('TITTIES');
-    });
-});
-
+getTime(time);
 
 
 
