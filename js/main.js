1f85d9c962857552b987476899b7771f6e5b89c3
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2016-09-05 12:23:32 -0400

    wee

--- js/main.js
+++ js/main.js
@@ -5,10 +5,13 @@ $(document).ready(function(){
 
 /* ------ LOAD MENUS ON CLICK ------- */
 
+
+
 var d = new Date();
 var time = d.toLocaleTimeString();
 console.log(time);
 
 
-
+$('#lunck').click(function(){
+	$('#target').load('/lunch-menu.html');
 });
