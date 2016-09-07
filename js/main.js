7c340581fbe77454ceeb07e5db9b59a252ee7e98
cinqueterreli <Anthony@Anthonys-MBP.home>
2016-09-07 15:34:49 -0400

    idk dude bro

--- js/main.js
+++ js/main.js
@@ -38,7 +38,22 @@ $('.menu-link').click(function(){
 $( window ).load( function()
 {
     $( '#grid' ).masonry( { itemSelector: '.grid-item' } );
+
+    var columns    = 3,
+        setColumns = function() { columns = $( window ).width() > 640 ? 3 : $( window ).width() > 320 ? 2 : 1; };
+ 
+    setColumns();
+    $( window ).resize( setColumns );
+ 
+    $( '#list' ).masonry(
+    {
+        itemSelector: '.item',
+        columnWidth:  function( containerWidth ) { return containerWidth / columns; }
+    });
+
 });
+
+
 
 
 
