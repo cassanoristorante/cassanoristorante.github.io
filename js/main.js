2c35faca642a71e3aefbca31670bf74a84a79e8f
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2016-11-16 21:08:35 -0500

    weee

--- js/main.js
+++ js/main.js
@@ -115,13 +115,22 @@ slider();
 
 
   $('a[href*="#"]:not([href="#"])').click(function() {
+
+
   if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
     var target = $(this.hash);
+
+    if ($(this).offset().top - target.offset().top > '600' || $(this).offset().top - target.offset().top < "-600"){
+    	var scrollSpeed = 1500;
+    } else {
+    	var scrollSpeed = 1000;
+    }
+
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
       $('html, body').animate({
         scrollTop: target.offset().top - 100
-      }, 1000);
+      }, scrollSpeed);
       return false;
     }
   }
