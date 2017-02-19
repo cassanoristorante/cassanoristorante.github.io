e2dcc179c1955820e0b38ccdfc8a2c4e2ff08002
cinqueterreli <Anthony@Anthonys-MBP.home>
2017-02-19 18:45:04 -0500

    maybe dis shit will work

--- js/main.js
+++ js/main.js
@@ -148,5 +148,40 @@ $('.nav-link').click(function() {
   $('.nav-link').not(this).removeClass('activate');
 });
 
+// ACTIVATE NAV LINKS ON SCROLL
+
+    var aChildren = $("nav li").children(); // find the a children of the list items
+    var aArray = []; // create the empty aArray
+    for (var i=0; i < aChildren.length; i++) {    
+        var aChild = aChildren[i];
+        var ahref = $(aChild).attr('href');
+        aArray.push(ahref);
+    } // this for loop fills the aArray with attribute href values
+
+    $(window).scroll(function(){
+        var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
+        var windowHeight = $(window).height(); // get the height of the window
+        var docHeight = $(document).height();
+
+        for (var i=0; i < aArray.length; i++) {
+            var theID = aArray[i];
+            var divPos = $(theID).offset().top; // get the offset of the div from the top of page
+            var divHeight = $(theID).height(); // get the height of the div in question
+            if (windowPos >= divPos && windowPos < (divPos + divHeight)) {
+                $("a[href='" + theID + "']").addClass("activate");
+            } else {
+                $("a[href='" + theID + "']").removeClass("activate");
+            }
+        }
+
+        if(windowPos + windowHeight == docHeight) {
+            if (!$("nav li:last-child a").hasClass("activate")) {
+                var navActiveCurrent = $(".activate").attr("href");
+                $("a[href='" + navActiveCurrent + "']").removeClass("activate");
+                $("nav li:last-child a").addClass("activate");
+            }
+        }
+    });
+
 
 });
