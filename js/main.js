73e834125b32e3808b22736fb74bf96bd788f47c
cinqueterreli <Anthony@Anthonys-MBP.home>
2017-02-18 19:58:24 -0500

    weeee

--- js/main.js
+++ js/main.js
@@ -17,11 +17,11 @@ console.log(time);
 if (time < 16){
 	$('#target').load('/lunch-menu.html');
   $(this).addClass('activated');
-  $('menu-link').children().not(this).removeClass('activated');
+  $('menu-nav').children().not(this).removeClass('activated');
 } else if (time >= 16){
 	$('#target').load('/dinner-menu.html');
   $(this).addClass('activated');
-  $('menu-link').children().not(this).removeClass('activated');
+  $('menu-nav').children().not(this).removeClass('activated');
 };
 
 
@@ -34,23 +34,23 @@ $('.menu-link').click(function(){
 	if (whichMenu == 'lunch'){
 		$('#target').load('/lunch-menu.html');
     $(this).addClass('activated');
-    $('menu-link').children().not(this).removeClass('activated');
+    $('menu-nav').children().not(this).removeClass('activated');
 	} else if (whichMenu == 'dinner'){
 		$('#target').load('/dinner-menu.html');
     $(this).addClass('activated');
-    $('menu-link').children().not(this).removeClass('activated');
+    $('menu-nav').children().not(this).removeClass('activated');
 	} else if (whichMenu == 'bar'){
 		$('#target').load('/bar-menu.html');
     $(this).addClass('activated');
-    $('menu-link').children().not(this).removeClass('activated');
+    $('menu-nav').children().not(this).removeClass('activated');
 	} else if (whichMenu == 'catering'){
 		$('#target').load('/catering-menu.html');
     $(this).addClass('activated');
-    $('menu-link').children().not(this).removeClass('activated');
+    $('menu-nav').children().not(this).removeClass('activated');
 	} else{
 		$('#target').load('/wine-menu.html');
     $(this).addClass('activated');
-    $('menu-link').children().not(this).removeClass('activated');
+    $('menu-nav').children().not(this).removeClass('activated');
 	}
  });
 
