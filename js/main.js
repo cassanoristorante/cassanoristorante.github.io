67a67c42d71999287cd073cfac5bb130c6307508
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2017-03-07 17:04:06 -0500

    LETS SEE

--- js/main.js
+++ js/main.js
@@ -1,6 +1,22 @@
 
 $(document).ready(function(){
 
+// HERO IMAGE
+
+function fullscreen(){
+        jQuery('#hero').css({
+            width: jQuery(window).width(),
+            height: jQuery(window).height()
+        });
+    }
+  
+    fullscreen();
+
+  // Run the function in case of window resize
+  jQuery(window).resize(function() {
+       fullscreen();         
+    });
+
 
 // CASSANO LINK ONCLICK SCROLL TO TOP
 
