dfd198c2d6afac3baaeeb8ad2efca23f6b95db9a
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2017-03-07 16:13:33 -0500

    weee

--- js/main.js
+++ js/main.js
@@ -93,7 +93,7 @@ $(window).scroll(function() {
        wH = $(window).height(),
        wS = $(this).scrollTop();
    if (wS > (hT+hH-wH)){
-       $(".menus-button").fadeIn(500);
+       $(".menus-button").css('visibility,' 'visible');
    }
 });
 
