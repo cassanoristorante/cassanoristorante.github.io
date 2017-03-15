d313c8c424838cd8d2288a53937c444527c7e515
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2017-03-15 03:26:21 -0400

    FIXED EET

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ $('.menu-link').click(function(){
 
 $("#menu-select").change(function() {
   var whichMenu = $(this).val();
-  console.log().whichMenu;
+  console.log(whichMenu);
   target = ('#target');
   if (whichMenu == 'lunch'){
     $(target).load('/lunch-menu.html');
