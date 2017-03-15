38d8eed90cac434809240c510e00bbc55658d5a6
cinqueterreli <Anthony@Anthonys-MacBook-Pro.local>
2017-03-14 20:28:26 -0400

    maybe i fix da code?

--- js/main.js
+++ js/main.js
@@ -303,19 +303,19 @@ $('.m-nav-link').click(function() {
 // GALLERY INITIALIZATION
 
 $('.picture').each( function() {
-    var $pic     = $(this),
+        var pic = $(this),
         getItems = function() {
             var items = [];
-            $pic.find('a').each(function() {
-                var $href   = $(this).attr('href'),
-                    $size   = $(this).data('size').split('x'),
-                    $width  = $size[0],
-                    $height = $size[1];
+            $(pic).find('a').each(function() {
+                var href   = $(this).attr('href'),
+                    size   = $(this).data('size').split('x'),
+                    width  = $(size)[0],
+                    height = $(size)[1];
  
                 var item = {
-                    src : $href,
-                    w   : $width,
-                    h   : $height
+                    src : $(href),
+                    w   : $(width),
+                    h   : $(height)
                 }
  
                 items.push(item);
@@ -327,13 +327,13 @@ $('.picture').each( function() {
 });
 
 
-var $pswp = $('.pswp')[0];
-$pic.on('click', 'figure', function(event) {
+var pswp = $('.pswp')[0];
+$(pic).on('click', 'figure', function(event) {
     event.preventDefault();
      
-    var $index = $(this).index();
+    var index = $(this).index();
     var options = {
-        index: $index,
+        index: $(index),
         bgOpacity: 0.7,
         showHideOpacity: true
     }
